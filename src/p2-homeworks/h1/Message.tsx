df0e048134317f48da3b06a7ee167ec34f57972f
k2 <izmail.k2@gmail.com>
2022-07-07 11:22:15 +0300

    HW 1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import "./Message.css";
 
-function Message() {
-    return (
-        <div>
-
-        </div>
-    )
+type MessageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
 }
 
-export default Message
+function Message(props: MessageType) {
+    return (
+        <div className="container">
+            <div className="container__avatar">
+                <img src={props.avatar} alt=""/>
+            </div>
+            <div className="angle"/>
+            <div className="container__message">
+                <h1>{props.name}</h1>
+                <p>{props.message}</p>
+                <div className="container__time">{props.time}</div>
+            </div>
+        </div>
+    );
+}
+
+export default Message;
